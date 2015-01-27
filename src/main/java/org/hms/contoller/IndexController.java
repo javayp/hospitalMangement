e14a6c092d12e14933899fbe51cd86034bbaa723
yp <prashanth.yp89@gmail.com>
2015-01-27 18:18:01 +0530

    Setup Commit

--- src/main/java/org/hms/contoller/IndexController.java
+++ src/main/java/org/hms/contoller/IndexController.java
@@ -1,5 +1,13 @@
 package org.hms.contoller;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String indexController(){
+		return "index";
+	}
 }
